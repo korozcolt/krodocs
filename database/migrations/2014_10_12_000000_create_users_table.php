c7d3af5617b6bff97b45e50f5c3ddb0b9330338b
Kristian Orozco <korozco@lean-tech.io>
2022-10-19 07:34:03 -0500

    advanced project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('first_name','60');
+            $table->string('last_name','60');
+            $table->string('phone','16');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
